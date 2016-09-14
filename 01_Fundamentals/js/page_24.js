cdbfa187a4cd7d058680ae44b29b511f2964dde1
Tommy Oh <tommyhyungjinoh@gmail.com>
2016-09-14 14:12:58 -0700

    page_24 for js has been completed.

--- 01_Fundamentals/js/page_24.js
+++ 01_Fundamentals/js/page_24.js
@@ -57,37 +57,132 @@
 /* ========================================================================== */
 // What Really Happened?
 
-function whatReallyHappensToday() {
-	var diaster_count = 0;
-	var chance = Math.floor(Math.random() * 100) + 1;
-	if (chance > 0 && chance <= 10) {
-		console.log('volcano');
-		diaster_count++;
+// function whatReallyHappensToday() {
+// 	var diaster_count = 0;
+// 	var chance = Math.floor(Math.random() * 100) + 1;
+// 	if (chance > 0 && chance <= 10) {
+// 		console.log('volcano');
+// 		diaster_count++;
+// 	}
+// 	chance = Math.floor(Math.random() * 100) + 1;
+// 	if (chance > 0 && chance <= 15) {
+// 		console.log('tsunami');
+// 		diaster_count++;
+// 	}
+// 	chance = Math.floor(Math.random() * 100) + 1;
+// 	if (chance > 0 && chance <= 20) {
+// 		console.log('earthquake');
+// 		diaster_count++;
+// 	}
+// 	chance = Math.floor(Math.random() * 100) + 1;
+// 	if (chance > 0 && chance <= 25) {
+// 		console.log('blizzard');
+// 		diaster_count++;
+// 	}
+// 	chance = Math.floor(Math.random() * 100) + 1;
+// 	if (chance > 0 && chance <= 30) {
+// 		console.log('meteor strike');
+// 		diaster_count++;
+// 	}
+// 	if (diaster_count == 0) {
+// 		console.log('nothing happend. Kenny will survive!');
+// 	}
+// }
+// whatReallyHappensToday();
+
+/* ========================================================================== */
+// Soaring IQ
+
+// function soaringIQ(iq) {
+// 	for (var i = 1, j = 98; i <= 98; i++) {
+// 		var increase = (i + 10000) * 0.0001;
+// 		iq *= increase;
+// 	}
+// 	return Math.round(iq);
+// }
+//
+// console.log(soaringIQ(101));
+
+/* ========================================================================== */
+// Letter Grade
+
+// function letterGrade(score) {
+// 	if (typeof(score) != 'number' || score > 100 || score < 0) {
+// 		return null;
+// 	}
+// 	if (score >= 90) {
+// 		console.log("Grade: A");
+// 	}
+// 	else if (score >= 80) {
+// 		console.log("Grade: B");
+// 	}
+// 	else if (score >= 70) {
+// 		console.log("Grade: C");
+// 	}
+// 	else if (score >= 60) {
+// 		console.log("Grade: D");
+// 	}
+// 	else {
+// 		console.log("Grade: F")
+// 	}
+// }
+// letterGrade(99);
+
+/* ========================================================================== */
+// More Accurate Grades
+
+function accurateGrade(score) {
+	if (typeof(score) != 'number' || score > 100 || score < 0) {
+		return null;
 	}
-	chance = Math.floor(Math.random() * 100) + 1;
-	if (chance > 0 && chance <= 15) {
-		console.log('tsunami');
-		diaster_count++;
+	if (score >= 90) {
+		if (score >= 98) {
+			console.log("Grade: A+");
+		}
+		else if (score <= 92) {
+			console.log("Grade: A-");
+		}
+		else {
+			console.log("Grade: A");
+		}
 	}
-	chance = Math.floor(Math.random() * 100) + 1;
-	if (chance > 0 && chance <= 20) {
-		console.log('earthquake');
-		diaster_count++;
+	else if (score >= 80) {
+		if (score >= 88) {
+			console.log("Grade: B+");
+		}
+		else if (score <= 82) {
+			console.log("Grade: B-");
+		}
+		else {
+			console.log("Grade: B");
+		}
 	}
-	chance = Math.floor(Math.random() * 100) + 1;
-	if (chance > 0 && chance <= 25) {
-		console.log('blizzard');
-		diaster_count++;
+	else if (score >= 70) {
+		if (score >= 78) {
+			console.log("Grade: C+");
+		}
+		else if (score <= 72) {
+			console.log("Grade: C-");
+		}
+		else {
+			console.log("Grade: C");
+		}
 	}
-	chance = Math.floor(Math.random() * 100) + 1;
-	if (chance > 0 && chance <= 30) {
-		console.log('meteor strike');
-		diaster_count++;
+	else if (score >= 60) {
+		if (score >= 68) {
+			console.log("Grade: D+");
+		}
+		else if (score <= 62) {
+			console.log("Grade: D-");
+		}
+		else {
+			console.log("Grade: D");
+		}
 	}
-	if (diaster_count == 0) {
-		console.log('nothing happend. Kenny will survive!');
+	else {
+		console.log("Grade: F")
 	}
 }
-whatReallyHappensToday();
+accurateGrade(99);
 
 /* ========================================================================== */
